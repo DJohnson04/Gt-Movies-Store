7f0221c2fd5bf6b0886d19435b46b980c8ced742
Kirito <kirito.machida@gmail.com>
2025-02-11 17:33:34 -0500

    added float field for prices

--- movies/models.py
+++ movies/models.py
@@ -2,7 +2,7 @@ from django.db import models
 class Movie(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
-    price = models.IntegerField()
+    price = models.FloatField()
     description = models.TextField()
     image = models.ImageField(upload_to='movie_images/')
     def __str__(self):
